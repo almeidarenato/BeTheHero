a909928ca2c700c675e82128c5d3d7328c906f59
almeidarenato <renatoam@ymail.com>
2020-03-25 22:29:36 -0300

    added logon page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 import "./styles.css";
-// import { Container } from './styles';
+
+import heroesImage from "../../assets/heroes.png";
+import LogoImage from "../../assets/logo.svg";
+import { FiLogIn } from "react-icons/fi";
 
 export default function Logon() {
-  return <div></div>;
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={LogoImage} alt="Be the Hero" />
+        <form>
+          <h1>Faça seu Logon</h1>
+          <input placeholder="Sua ID" />
+          <button className="button">Entrar</button>
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não Tenho Cadastro
+          </a>
+        </form>
+      </section>
+
+      <img src={heroesImage} alt="Heroes Logo" />
+    </div>
+  );
 }
