f638e39341edfb53ef83393ef5da4988e61a2b46
almeidarenato <renatoam@ymail.com>
2020-03-27 19:56:09 -0300

    created Mobile project and incidents page

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Detail() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default Detail;
